00cd1a4c5ffe036ff08bf2a4cbdb0ffedc40a388
rfoecking <captainfoecking@gmail.com>
2017-04-02 12:41:27 -0400

    cat prefab, deleting things

---
+++ Assets/scripts/AllItems.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllItems : ScriptableObject {
+
+
+    List<Item> allItems = new List<Item>();
+
+}
